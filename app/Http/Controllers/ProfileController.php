776b5bce88c332891512e772392b3b1fd6e055c8
paulhenry <e-paul@chronostep.com>
2024-10-10 16:36:38 +0800

    adding profile image

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
+use Illuminate\Support\Str;
 
 class ProfileController extends Controller
 {
@@ -24,17 +25,22 @@ class ProfileController extends Controller
     /**
      * Update the user's profile information.
      */
-    public function update(ProfileUpdateRequest $request): RedirectResponse
+    public function update(ProfileUpdateRequest $request)
     {
-        $request->user()->fill($request->validated());
+        $user = auth()->user();
+        $user->update($request->validated());
 
-        if ($request->user()->isDirty('email')) {
-            $request->user()->email_verified_at = null;
+        if ($request->hasFile('photo')) {
+            $fileExtension = $request->file('photo')->getClientOriginalExtension();
+            $fileName = Str::random(40) . '.' . $fileExtension;
+
+            $filePath = $request->file('photo')->storeAs('users', $fileName, 'public');
+
+            // Update the user's photo column with just the file name
+            $user->update(['photo' => $fileName]);
         }
 
-        $request->user()->save();
-
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
+        return redirect()->back()->with('success', 'Profile updated successfully.');
     }
 
     /**
